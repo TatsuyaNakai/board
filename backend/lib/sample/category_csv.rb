92f8eef7aba739ab774e5e23a21413835cc1ce12
tatsuya.nakai <15g079nt@gmail.com>
2023-04-02 20:55:07 +0900

    fix_rubocop

--- backend/lib/sample/category_csv.rb
+++ backend/lib/sample/category_csv.rb
@@ -5,11 +5,9 @@ module Sample
     def self.load
       ActiveRecord::Base.transaction do
         result = CSV.read('./lib/sample/csv/category.csv', headers: true).map(&:to_hash)
-  
+
         result.each do |one|
-          Category.create!(
-            name: one['カテゴリ名'],
-          )
+          Category.create!(name: one['カテゴリ名'])
         end
       end
     end
