2741ab23a38b4b4f79913a29a619027e45922864
tatsuya.nakai <15g079nt@gmail.com>
2023-04-03 08:29:24 +0900

    ディレクトリ構成の修正とクエリの追加

---
+++ backend/spec/graphql/queries/current_admin_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe 'QueryType#current_admin' do
+  before do
+    @query = <<-QUERY
+      query currentAdmin {
+        currentAdmin {
+          id
+          accessToken
+        }
+      }
+    QUERY
+  end
+
+  it 'は、ログイン情報を取得できること' do
+    admin = Admin.first
+    result = MyappSchema.execute(@query, context: { current_admin: admin })
+    admin_result = result['data']['currentAdmin']
+
+    admin_result.each do |key, value|
+      value = value.to_i if key == 'id'
+      expect(value).to eq admin[key.underscore]
+    end
+  end
+
+  it 'は、ログイン情報を取得できないこと' do
+    result = MyappSchema.execute(@query)
+    query_result = result['errors'].first
+
+    expect(query_result['message']).to eq 'UNAUTHENTICATED'
+  end
+end
---
+++ backend/spec/graphql/queries/login_admin_spec.rb
@@ -0,0 +1,44 @@
+RSpec.describe 'QueryType#login_admin' do
+  before do
+    @query = <<-QUERY
+      query loginAdmin($email: String, $password: String) {
+        loginAdmin(email: $email, password: $password) {
+          admin {
+            id
+            accessToken
+          }
+          result
+          errors {
+            attribute
+            messages
+          }
+          fullMessages
+        }
+      }
+    QUERY
+  end
+
+  it 'は、管理者情報を取得できること' do
+    variables = { email: '15g079nt@gmail.com', password: 'password' }
+    result = MyappSchema.execute(@query, variables: variables)
+    admin = Admin.first
+    query_result = result['data']['loginAdmin']['result']
+    admin_result = result['data']['loginAdmin']['admin']
+
+    expect(query_result).to eq true
+    admin_result.each do |key, value|
+      value = value.to_i if key == 'id'
+      expect(value).to eq admin[key.underscore]
+    end
+  end
+
+  it 'は、管理者情報を取得できないこと' do
+    variables = { email: '', password: '' }
+    result = MyappSchema.execute(@query, variables: variables)
+    query_result = result['data']['loginAdmin']['result']
+    errors_result = result['data']['loginAdmin']['fullMessages'].first
+
+    expect(query_result).to eq false
+    expect(errors_result).to eq 'パスワードが間違っています、または、登録されていないEメールです。'
+  end
+end
